f5007a00cd5ac99a454d40831b31a5bccaa46986
Jtmz05 <158003640+Jtmz05@users.noreply.github.com>
2025-11-21 22:40:17 -0600

    Add files via upload

--- Practica9/Practica9.py
+++ Practica9/Practica9.py
@@ -1,1 +1,25 @@
+import pandas as pd
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
+
+df = pd.read_excel("superstore_clean.practica2_reporte.xlsx")
+
+
+columna_texto = "Category"
+
+
+texto = " ".join(df[columna_texto].astype(str))
+
+# Generar Word Cloud
+wordcloud = WordCloud(width=1000, height=500, background_color="white").generate(texto)
+
+# Mostrar Word Cloud
+plt.figure(figsize=(12,6))
+plt.imshow(wordcloud, interpolation="bilinear")
+plt.axis("off")
+plt.show()
+
+
+wordcloud.to_file("practica9.png")
+print("Word Cloud generado ")
